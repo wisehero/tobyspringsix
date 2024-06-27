b24d4fb6bef614f68e5c000e9fa764484c92eba2
wisehero <godjw96@gmail.com>
2024-06-28 03:30:31 +0900

    # 토비의 스프링 6 시작

---
+++ src/main/java/com/example/tobyspringsix/TobyspringsixApplication.java
@@ -0,0 +1,13 @@
+package com.example.tobyspringsix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobyspringsixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TobyspringsixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tobyspringsix/TobyspringsixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tobyspringsix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobyspringsixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
