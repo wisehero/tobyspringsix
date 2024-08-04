6780ae67e2078dad8915ff2c05522fa990fa1137
wisehero <godjw96@gmail.com>
2024-08-04 14:01:58 +0900

    # 트랜잭션 서비스 추상화

--- src/main/java/com/example/tobyspringsix/DataConfig.java
+++ src/main/java/com/example/tobyspringsix/DataConfig.java
@@ -12,6 +12,7 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.support.PersistenceAnnotationBeanPostProcessor;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import jakarta.persistence.EntityManagerFactory;
 
@@ -58,7 +59,7 @@ public class DataConfig {
 	 */
 
 	@Bean
-	public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+	public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
 		return new JpaTransactionManager(emf);
 	}
 }
--- src/main/java/com/example/tobyspringsix/order/OrderService.java
+++ src/main/java/com/example/tobyspringsix/order/OrderService.java
@@ -2,23 +2,23 @@ package com.example.tobyspringsix.order;
 
 import java.math.BigDecimal;
 
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
 
 /**
- * 현재 아래의 애플리케이션 서비스는 JPA에 의존하고 있음
- * JPA를 사용하는 Repository 클래스에 의존하고 있음
- * JPA Transaction Manager에 의존하고 있음
+ * 현재 아래의 애플리케이션 서비스는 JPA에 의존하고 있음 -> 해결.
+ * JPA를 사용하는 Repository 클래스에 의존하고 있음 -> 해결. OrderRepository 도입
+ * JPA Transaction Manager에 의존하고 있음 -> PlatformTransactionManager를 도입하여 해결.
  *
  */
 @Service
 public class OrderService {
 
 	private final OrderRepository orderRepository;
-	private final JpaTransactionManager transactionManager;
+	private final PlatformTransactionManager transactionManager;
 
-	public OrderService(OrderRepository orderRepository, JpaTransactionManager transactionManager) {
+	public OrderService(OrderRepository orderRepository, PlatformTransactionManager transactionManager) {
 		this.orderRepository = orderRepository;
 		this.transactionManager = transactionManager;
 	}
